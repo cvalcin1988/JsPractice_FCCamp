422b7ab4fcc98401326b819667e2587b809811bf
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-30 15:36:03 -0500

    more declaration-assignments-properCamelCase

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var a = 5;
-var b = 10;
-var c = "I am a";
+//Case Sensitivity
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
