65a72f1fa2629e6167c87d9432a9324b8aa23be3
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-30 15:47:48 -0500

    Compound Assignment, 18:25

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-//Case Sensitivity
+/*
+//Addition
+var sum = 10 + 10;
+console.log(sum);
 
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+//Subtraction
 
-// Assignments
+var difference = 45-33;
+console.log(difference);
 
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+//Multiplication
+
+var product = 8 * 10;
+console.log(product);
+
+//Division
+
+var quotient = 66 / 33;
+console.log(quotient);
+
+//Increment
+var myVar = 87;
+myVar++;
+
+//Decrement
+var myVar = 11;
+
+myVar--;
+console.log(myVar);
+*/
+
+//Decimal Numbers
+
+// var ourDecimal = 5.7;
+// var myDecimal = 0.009;
+
+// var product = 2.0 * 2.5;
+
+// console.log(product);
+
+// //Divide Decimal numbers
+
+// var quotient = 4.4 / 2.0;
+
+//Finding a Remainder
+
+// var remainder;
+// remainder = 11 % 3;
+// console.log(remainder);
+
+//Compound Assignment with Augmented Addition 18:24
