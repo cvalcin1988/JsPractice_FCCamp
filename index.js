45a8a22fb60f46c7190c96793c6bb125486b4783
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-30 15:18:37 -0500

    Data Types and ways to declare a variable

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 console.log("Hello from Javascript");
 
-// in-line comment
+/* Data Types
+undefined, null, boolean, string, symbol, number, and object
+something that hasnt been defined
+is nothing
+true/false
+immutable primitive value thats unique
+
+key value pairs
+*/
+
+var myName = "Beau";    //used throughout whole program
+myName = 8;
+
+let ourName = "freeCodeCamp"    //will only be used within the scope you declared
+
+const pi = 3.14     // variable that can never be changed
 /*
-var number = 5;
-let number = 8
+There are 3 ways to declare a variable in js
+var is one, let is another
 */
