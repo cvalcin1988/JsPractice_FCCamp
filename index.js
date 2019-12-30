0805db4e0a9d8089fe21e5c63e7b479e07c042f3
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-30 15:23:11 -0500

    declaring and assigning

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-console.log("Hello from Javascript");
+//semicolons are not required but prefered
+var a; //declare a variable
+var b = 2; //declare and assign
 
-/* Data Types
-undefined, null, boolean, string, symbol, number, and object
-something that hasnt been defined
-is nothing
-true/false
-immutable primitive value thats unique
+console.log(a);
+a = 7;
 
-key value pairs
-*/
+b = a;
 
-var myName = "Beau";    //used throughout whole program
-myName = 8;
-
-let ourName = "freeCodeCamp"    //will only be used within the scope you declared
-
-const pi = 3.14     // variable that can never be changed
-/*
-There are 3 ways to declare a variable in js
-var is one, let is another
-*/
+console.log(a);
