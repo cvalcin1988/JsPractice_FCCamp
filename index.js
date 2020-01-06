867c9b417e4c2c0a0c3a337b8ea1176d5c66b10a
Calherbe Valcin <cvalcin1988@gmail.com>
2020-01-06 15:37:27 -0500

    Declare Strings - Concatenating Strings with Plus Equals Operator ~ 29:02

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-// Compound Assignment with Augmented Subtraction  19:22
+// Declare String Variables
 
+// var firstName = "Alan"; //string literal
+// var lastName = "Turing";
 
-// var a = 11;
-// var b = 9;
-// var c = 3;
+// var myFirstName = "Beau";
+// var myLastName = "Carnes";
 
-// // Only modify code below this line
+// Escaping Literal Quotes in Strings ~ 22:01
 
-// a -= 6;
-// b -= 15;
-// c -= 1;
-// console.log(a);
+// var myStr = "I am a \"double quoted\" string inside \"double quotes\"\.";
+// //The backslash in front of the quotation mark indicate it isnt the end of the string
+// console.log(myStr);
 
-// Compound Assignment with Augmented Multiplication ~ 20:20
+// Quoting Strings with Single Quotes
 
-// var a = 5;
-// var b = 12;
-// var c = 4.6;
+// var myStr = '<a href= "http://www.example.com" target="_blank">Link</a>';
+// single quotes also allow you to use quotes within a string
+// var myStr = `'<a href= "http://www.example.com" target="_blank">Link</a>'`;
+// backticks (`) allow you to use the ('') and ("") marks in a string
+// console.log(myStr);
 
-// // Only modify code below this line
+// Escape Sequences in Strings ~ 25:19
+/*
+Code Output
+\' single quote
+\" double quote
+\\ backslash
+\n new line
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+` backticks before '
+*/
 
-// a *= 5;
-// b *= 3;
-// c *= 10;
-// console.log(a);
+// var myStr = "FirstLine\n \t\\SecondLine\nThirdLine";
+// console.log(myStr);
 
-// Compound Assignment with Augmented Division ~ 20:52
+// Concatenating Strings with Plus Operator ~ 26:47
+//Example
+// var ourString = "I come first. " + "I come second";
+// Only change code below this line
 
-// var a = 48;
-// var b = 108;
-// var c = 33;
+// var myStr = "This is the start. " + "This is the end";
+// console.log(ourString);
 
-// // Only modify code below this line
+// Concatenating Strings with Plus Equals Operator
 
-// a /= 12;
-// b /= 4;
-// c /= 11;
-// console.log(b);
+// var ourString = "I come first. ";
+// ourString += "I come second.";
+// console.log(ourString);
+
+// var myStr = "This is the first sentence. ";
+// myStr += "\n\tThis is the second sentence."
+// console.log(myStr);
