70d943d44504b035c8daad1fdfa1cd89efde7050
Calherbe Valcin <cvalcin1988@gmail.com>
2020-01-06 22:26:26 -0500

    Return a value from a function with Return 1:02:42

--- index.js
+++ index.js
@@ -1,67 +1,102 @@
-// Shopping List ~ 50:37
+// Global vs Local Scope in Functions ~ 1:00:45
+/*
+var outerWear = "T-Shirt";
 
-// var myList = [["cereal", 3]["milk", 2]["bananas", 3]["juice", 2]["eggs", 12]];
+function myOutfit()
+{
+    var outerWear = "sweater";
+    return outerWear;
+}
+console.log(myOutfit());
+console.log(outerWear);
+*/
 
-// Write Reusable Code with Functions ~ 51:42
-// function ourReusableFunction()
-// {
-//     console.log("Heyya, World");
-// }
-// ourReusableFunction();
+// Return a Value from a Function with Return 1:02:42
 
-// function reusableFunction()
-// {
-//     console.log("Hi World");
-// }
-// reusableFunction(); 
+//  (1:03:55) 53. Understanding Undefined Value returned from a Function
+//  (1:04:52) 54. Assignment with a Returned Value
+//  (1:05:52) 55. Stand in Line
+//  (1:08:41) 56. Understanding Boolean Values
+//  (1:09:24) 57. Use Conditional Logic with If Statements
+//  (1:11:51) 58. Comparison with the Equality Operator
+//  (1:13:18) 59. Comparison with the Strict Equality Operator
+//  (1:14:43) 60. Practice comparing different values
+//  (1:15:38) 61. Comparison with the Inequality Operator
+//  (1:16:20) 62. Comparison with the Strict Inequality Operator
+//  (1:17:05) 63. Comparison with the Greater Than Operator
+//  (1:17:39) 64. Comparison with the Greater Than Or Equal To Operator
+//  (1:18:09) 65. Comparison with the Less Than Operator
+//  (1:18:44) 66. Comparison with the Less Than Or Equal To Operator
+//  (1:19:17) 67. Comparisons with the Logical And Operator
+//  (1:20:41) 68. Comparisons with the Logical Or Operator
+//  (1:21:37) 69. Introducing Else Statements
+//  (1:22:27) 70. Introducing Else If Statements
+//  (1:23:30) 71. Logical Order in If Else Statements
+//  (1:24:45) 72. Chaining If Else Statements
+//  (1:27:45) 73. Golf Code
+//  (1:32:15) 74. Selecting from Many Options with Switch Statements
+//  (1:35:46) 75. Adding a Default Option in Switch Statements
+//  (1:37:23) 76. Multiple Identical Options in Switch Statements
+//  (1:39:20) 77. Replacing If Else Chains with Switch
+//  (1:41:11) 78. Returning Boolean Values from Functions
+//  (1:42:20) 79. Return Early Pattern for Functions
+//  (1:43:38) 80. Counting Cards
+//  (1:49:11) 81. Build JavaScript Objects
+//  (1:50:46) 82. Accessing Object Properties with Dot Notation
+//  (1:51:33) 83. Accessing Object Properties with Bracket Notation
+//  (1:52:47) 84. Accessing Object Properties with Variables
+//  (1:53:34) 85. Updating Object Properties
+//  (1:54:30) 86. Add New Properties to a JavaScript Object
+//  (1:55:19) 87. Delete Properties from a JavaScript Object
+//  (1:55:54) 88. Using Objects for Lookups
+//  (1:57:43) 89. Testing Objects for Properties
+//  (1:59:15) 90. Manipulating Complex Objects
+//  (2:01:00) 91. Accessing Nested Objects
+//  (2:01:53) 92. Accessing Nested Arrays
+//  (2:03:06) 93. Record Collection
+//  (2:10:15) 94. Iterate with JavaScript While Loops
+//  (2:11:35) 95. Iterate with JavaScript For Loops
+//  (2:13:56) 96. Iterate Odd Numbers With a For Loop
+//  (2:15:28) 97. Count Backwards With a For Loop
+//  (2:17:08) 98. Iterate Through an Array with a For Loop
+//  (2:19:43) 99. Nesting For Loops
+//  (2:22:45) 100. Iterate with JavaScript Do...While Loops
+//  (2:24:12) 101. Profile Lookup
+//  (2:28:18) 102. Generate Random Fractions with JavaScript
+//  (2:28:54) 103. Generate Random Whole Numbers with JavaScript
+//  (2:30:21) 104. Generate Random Whole Numbers within a Range
+//  (2:31:46) 105. Use the parseInt Function
+//  (2:32:36) 106. Use the parseInt Function with a Radix
+//  (2:33:29) 107. Use the Conditional (Ternary) Operator
+//  (2:34:57) 108. Use Multiple Conditional (Ternary) Operators
 
-// Passing Values to Functions with Arguments
+// PART 2: ES6
+//  (2:36:57) 109. Explore Differences Between the var and let Keywords
+//  (2:39:02) 110. Compare Scopes of the var and let Keywords
+//  (2:41:32) 111. Declare a Read-Only Variable with the const Keyword
+//  (2:43:40) 112. Mutate an Array Declared with const
+//  (2:44:52) 113. Prevent Object Mutation
+//  (2:47:17) 114. Use Arrow Functions to Write Concise Anonymous Functions
+//  (2:28:24) 115. Write Arrow Functions with Parameters
+//  (2:49:27) 116. Write Higher Order Arrow Functions
+//  (2:53:04) 117. Set Default Parameters for Your Functions
+//  (2:54:00) 118. Use the Rest Operator with Function Parameters
+//  (2:55:31) 119. Use the Spread Operator to Evaluate Arrays In-Place
+//  (2:57:18) 120. Use Destructuring Assignment to Assign Variables from Objects
+//  (3:00:18) 121. Use Destructuring Assignment to Assign Variables from Nested Objects
+//  (3:01:55) 122. Use Destructuring Assignment to Assign Variables from Arrays
+//  (3:03:40) 123. Use Destructuring Assignment with the Rest Operator to Reassign Array
+//  (3:05:05) 124. Use Destructuring Assignment to Pass an Object as a Function's
+//  (3:06:39) 125. Create Strings using Template Literals
+//  (3:10:43) 126. Write Concise Object Literal Declarations Using Simple Fields
+//  (3:12:24) 127. Write Concise Declarative Functions with ES6
+//  (3:12:56) 128. Use class Syntax to Define a Constructor Function
+//  (3:15:11) 129. Use getters and setters to Control Access to an Object
+//  (3:20:25) 130. Understand the Differences Between import and require
+//  (3:22:33) 131. Use export to Reuse a Code Block
+//  (3:23:40) 132. Use * to Import Everything from a File
+//  (3:24:50) 133. Create an Export Fallback with export default
+//  (3:25:26) 134. Import a Default Export
 
-// function ourFunctionWithArgs(a,b)
-// {
-//     console.log(a - b);
-// }
-// ourFunctionWithArgs(10,5);
 
-// function functionWithArgs (a,b)
-// {
-//     console.log(a + b);
-// }
-// functionWithArgs(10,5);
-
-// Global Scope and Functions
-
-// var myGlobal = 10;
-
-// function fun1()
-// {
-//     oopsGlobal = 5;
-//     // By not putting the var keyword, this variable is global by default
-//     // var will make the variable limited to its scope
-// }
-
-// function fun2()
-// {
-//     var output = "";
-//     if (typeof myGlobal != "undefined")
-//     {
-//         output += "myGlobal: " + myGlobal;
-//     }
-//     if (typeof oopsGlobal != "undefined")
-//     {
-//         output += " oopsGlobal: " + oopsGlobal;
-//     }
-//     console.log(output);
-// }
-// fun1();
-// fun2();
-
-// Local Scope and Functions ~ 59:32
-
-function myLocalScope()
-    {
-        var myVar = 5;
-        console.log(myVar);
-    }
-    myLocalScope();
 
